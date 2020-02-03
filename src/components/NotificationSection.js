32d4f1fdb88e23774fca5fc815e11877c9e4b2ab
Eddo <eddo@nadyne.com>
2020-02-03 19:05:12 +0700

    cookies

--- src/components/NotificationSection.js
+++ src/components/NotificationSection.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Notification = (props) => {
   return (
-    <div></div>
+    <section id="js-cookie-box" className="cookie-box cookie-box--hide">
+        <div className="notification-panel">
+        <p> By accessing and using this Website, you acknowledge that you have read and <br></br> understand our <b>Cookie Policy, Privacy Policy, </b>and our<b> Terms of Service</b> <span id="js-cookie-button" className="cookie-button cookiebtn">Got it</span></p>
+        </div>
+    </section>  
   );
 }
 export default Notification;
