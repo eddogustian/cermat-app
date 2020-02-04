50c77da3acf28fab6b0113564aa55dfd1fb3bf99
Eddo <eddo@nadyne.com>
2020-02-04 18:01:23 +0700

    clean

--- src/components/NotificationSection.js
+++ src/components/NotificationSection.js
@@ -1,27 +1,24 @@
 import React from 'react';
-// import Cookies from 'js-cookie';
-import { Cookies } from 'react-cookie';
+import CookieConsent, { Cookies } from "react-cookie-consent";
+
 
 const Notification = (props) => {
   const handleCookies = () => {
-    const cookieBox = document.getElementById('js-cookie-box');
-    const cookieButton = document.getElementById('js-cookie-button');
-
-    if (!Cookies.get('cookie-box')) {
-      cookieBox.remove('cookie-box--hide');
-      
-      cookieButton.onclick = function() {
-        Cookies.set('cookie-box', true, { expires: 7 });
-        cookieBox.add('cookie-box--hide');
-      }
-    }
+    Cookies.set("test", "nice")
   }
   return (
-    <section id="js-cookie-box" className="cookie-box">
-        <div className="notification-panel">
-        <p> By accessing and using this Website, you acknowledge that you have read and <br></br> understand our <b>Cookie Policy, Privacy Policy, </b>and our<b> Terms of Service</b> <span id="js-cookie-button" className="cookie-button cookiebtn" onClick={handleCookies} >Got it</span></p>
-        </div>
-    </section>  
+    <CookieConsent
+      disableStyles={true}
+      location="top"
+      expires={150}
+      buttonText="GOT IT"
+      buttonClasses="btn btn-primary"
+      containerClasses="col-lg-12"
+      contentClasses="text-capitalize"
+    >
+      By accessing and using this Website, you acknowledge that you have read and <br></br> understand our <b>Cookie Policy, Privacy Policy, </b>and our<b> Terms of Service</b>
+  </CookieConsent>
   );
 }
 export default Notification;
+
