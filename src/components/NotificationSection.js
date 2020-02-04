d25b0c24a701b28aa40826d5c0ae51477cd224f5
Eddo <eddo@nadyne.com>
2020-02-04 18:22:46 +0700

    css

--- src/components/NotificationSection.js
+++ src/components/NotificationSection.js
@@ -7,9 +7,23 @@ const Notification = (props) => {
     Cookies.set("test", "nice")
   }
   return (
+
+  //   <CookieConsent
+  //     disableStyles={true}
+  //     location="top"
+  //     expires={150}
+  //     buttonText="Got it"
+  //     buttonClasses="cookie-button"
+  //     containerClasses="col-lg-12"
+  //     contentClasses="text-capitalize"
+  //   >
+  //     By accessing and using this Website, you acknowledge that you have read and <br></br> understand our <b>Cookie Policy, Privacy Policy, </b>and our<b> Terms of Service</b>
+  // </CookieConsent>
+
+
     <CookieConsent
-      disableStyles={true}
       location="top"
+      style={{ position: "unset" }}
       expires={150}
       buttonText="GOT IT"
       buttonClasses="btn btn-primary"
