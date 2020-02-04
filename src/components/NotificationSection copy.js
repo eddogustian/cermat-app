ce70ee6269f00935e0d2d8a253a28ffd03e13be7
Eddo <eddo@nadyne.com>
2020-02-04 14:11:15 +0700

    cookies

--- src/components/NotificationSection copy.js
+++ src/components/NotificationSection copy.js
@@ -1,23 +1,28 @@
-import React from 'react';
-// import Cookies from 'js-cookie';
-import { Cookies } from 'react-cookie';
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
 
 const Notification = (props) => {
+  const [hideClass, setHideClass] = useState('');
   const handleCookies = () => {
     const cookieBox = document.getElementById('js-cookie-box');
     const cookieButton = document.getElementById('js-cookie-button');
-
+    setHideClass('cookie-box--hide');
+    console.log(hideClass);
     if (!Cookies.get('cookie-box')) {
-      cookieBox.remove('cookie-box--hide');
+      
+    cookieBox.classList.remove('cookie-box--hide');
       
       cookieButton.onclick = function() {
+        
         Cookies.set('cookie-box', true, { expires: 7 });
-        cookieBox.add('cookie-box--hide');
+       
+        cookieBox.remove('#cookie-box',true,);
+        cookieBox.classList.add('cookie-box--hide');
       }
     }
   }
   return (
-    <section id="js-cookie-box" className="cookie-box">
+    <section id="js-cookie-box" className={`cookie-box ${hideClass}`}>
         <div className="notification-panel">
         <p> By accessing and using this Website, you acknowledge that you have read and <br></br> understand our <b>Cookie Policy, Privacy Policy, </b>and our<b> Terms of Service</b> <span id="js-cookie-button" className="cookie-button cookiebtn" onClick={handleCookies} >Got it</span></p>
         </div>
