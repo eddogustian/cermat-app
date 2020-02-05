26c1522e366e59deed97075f0cd485a772c9416b
Eddo <eddo@nadyne.com>
2020-02-05 19:07:08 +0700

    notice

--- src/components/NotificationSection.js
+++ src/components/NotificationSection.js
@@ -4,26 +4,14 @@ import CookieConsent from "react-cookie-consent";
 
 const Notification = (props) => {
   return (
-
-  //   <CookieConsent
-  //     disableStyles={true}
-  //     location="top"
-  //     expires={150}
-  //     buttonText="Got it"
-  //     buttonClasses="cookie-button"
-  //     containerClasses="col-lg-12"
-  //     contentClasses="text-capitalize"
-  //   >
-  //     By accessing and using this Website, you acknowledge that you have read and <br></br> understand our <b>Cookie Policy, Privacy Policy, </b>and our<b> Terms of Service</b>
-  // </CookieConsent>
-
-
     <CookieConsent
       location="top"
       style={{ position: "unset" }}
       expires={150}
-      buttonText="GOT IT"
-      buttonClasses="btn btn-primary"
+      style={{ background: "#e5e5e5", color: "#212529" }}
+      buttonText="Got it"
+      disableButtonStyles="true"
+      buttonClasses="cookie-button"
       containerClasses="col-lg-12"
       contentClasses="text-capitalize"
     >
