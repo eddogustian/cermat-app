184b48d17276bfbee3f0c7e47009b18f6d3cada3
Eddo <eddo@nadyne.com>
2020-02-05 19:27:29 +0700

    notice

--- src/components/NotificationSection.js
+++ src/components/NotificationSection.js
@@ -5,10 +5,11 @@ import CookieConsent from "react-cookie-consent";
 const Notification = (props) => {
   return (
     <CookieConsent
+      // disableStyles={true}
       location="top"
       style={{ position: "unset" }}
       expires={150}
-      style={{ background: "#e5e5e5", color: "#212529" }}
+      style={{ background: "#e5e5e5", color: "#212529", alignitems: "left" }}
       buttonText="Got it"
       disableButtonStyles="true"
       buttonClasses="cookie-button"
